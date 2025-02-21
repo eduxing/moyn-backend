96b1d9cb5165be998ec83d51dfd5403ff09bd0ff
Eduardo Escarti <eduardo.escarti@xing.com>
2025-02-21 10:21:36 +0100

    add profile_url to the admin

--- moyn_user/admin.py
+++ moyn_user/admin.py
@@ -2,10 +2,9 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import MoynUser
 
-
 class MoynUserAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('phone_number', 'address')}),
+        (None, {'fields': ('phone_number', 'address', 'profile_url')}),
     )
 
 admin.site.register(MoynUser, MoynUserAdmin)
